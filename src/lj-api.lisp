680fb19462e3f596514d5c3ee3ba53b4db9e26dd
Dmitry Petrov <dpetroff@gmail.com>
2016-09-15 01:24:03 +0200

    functions are not used anymore, removing

--- src/lj-api.lisp
+++ src/lj-api.lisp
@@ -35,57 +35,12 @@
                        "auth_challenge" challenge
                        "auth_response" auth-response))))
 
-(defun add-mask (req)
-  (if (find "usemask" req :test #'equal)
-      (concatenate 'list
-                   req
-                   (list "allowmask" 1))
-      req))
-
-
 (defun login ()
   (let* ((struct (apply #'s-xml-rpc:xml-rpc-struct (add-challenge)))
          (request (s-xml-rpc:encode-xml-rpc-call "LJ.XMLRPC.login" struct)))
     (s-xml-rpc:xml-rpc-call request
                             :url "/interface/xmlrpc"
                             :host "www.livejournal.com")))
-
-(defun add-props (plist)
-  (let* ((file-fields (list :music :mood :location :tags))
-         (lj-fields (list "current_music" "current_mood" "current_location" "taglist"))
-         (props (mapcar #'(lambda (field lj-field)
-                            (if (getf plist field)
-                                (list lj-field (getf plist field)) ()))
-                        file-fields lj-fields)))
-    (apply #'s-xml-rpc:xml-rpc-struct
-           (apply #'concatenate 'list props))))
-
-(defun editevent (plist)
-  (let* ((params (list "event" (getf plist :body)
-                       "subject" (or (getf plist :title) "")
-                       "security" (get-privacy-setting plist)
-                       "itemid" (getf plist :itemid)
-                       "year" (getf plist :year)
-                       "mon" (getf plist :mon)
-                       "day" (getf plist :day)
-                       "hour" (getf plist :hour)
-                       "min" (getf plist :min)
-                       "props" (add-props plist)))
-         (struct (apply #'s-xml-rpc:xml-rpc-struct (add-challenge (add-mask params))))
-         (request (s-xml-rpc:encode-xml-rpc-call "LJ.XMLRPC.editevent" struct)))
-    (s-xml-rpc:xml-rpc-call request
-                            :url "/interface/xmlrpc"
-                            :host "www.livejournal.com")))
-
-(defun get-privacy-setting (plist)
-  (let ((privacy (or (getf plist :privacy) "public")))
-    (cond
-      ((string= privacy "public") "public")
-      ((string= privacy "friends") "usemask")
-      ((string= privacy "private") "private")
-      (t "public")
-      )))
-
 
 (defgeneric create-new-post (post))
 
